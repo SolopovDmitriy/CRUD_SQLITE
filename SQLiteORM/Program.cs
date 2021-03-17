b3b10a36f5512f57ecf6ee0fbccc8c0c5ca8647a
Dmitriy Solopov <div.solopov@gmail.com>
2021-03-17 23:53:05 +0200

    first commit

---
+++ SQLiteORM/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                /*SQLiteConnector.CreateDatabaseSource("test.db");
+                SQLiteConnector.Connection.Open();
+                string createStudentTableQuery = "CREATE TABLE IF NOT EXISTS students (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, fio VARCHAR(128) NOT NULL, age INTEGER )";
+                    SQLiteCommand sQLiteCommand = new SQLiteCommand(createStudentTableQuery, SQLiteConnector.Connection);
+                sQLiteCommand.ExecuteNonQuery();
+                SQLiteConnector.Connection.Close();*/
+
+                string pathTofile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "database", "test.db");
+                SQLiteDBEngine dBEngine = new SQLiteDBEngine(pathTofile, SQLIteMode.EXISTS);
+
+
+                Console.WriteLine(dBEngine["student"].Name);
+
+                SQLiteTable Students = dBEngine["student"];
+                foreach (SQLiteColumn col in Students.HeadRowInfo)
+                {
+                    Console.WriteLine(col);
+                }
+
+                // Students.RemoveRowById(6);
+                //Console.WriteLine("name of table is "+ Students.Name );
+
+
+                //// create 
+                //List<string> newStudent = new List<string>();
+                //newStudent.Add("Petr5");
+                //newStudent.Add("45");
+                //Students.CreateRow(newStudent);
+
+
+                // update 
+                List<string> newStudent = new List<string>();
+                newStudent.Add("aaaaa");
+                newStudent.Add("100");
+                Students.UpdateRow(16, newStudent);
+
+              
+                Console.WriteLine("Данные таблицы");
+                foreach (var col in Students.BodyRows)
+                {
+                    Console.WriteLine($"ID: {col.Key}");
+
+                    Console.WriteLine("size of col.Value = " + col.Value.Count);
+                    foreach (var item in col.Value)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("======================");
+                }
+
+                /*foreach (var item in dBEngine.Tables)
+                {
+                    Console.WriteLine(item);
+                }
+                dBEngine.getTableFields("student");*/
+
+                //string[] str = { };
+                /*SQLiteColumn sQLiteColumn = new SQLiteColumn(str);
+                //sQLiteColumn.GetDateType("VARCHAR(128)");
+                Console.WriteLine(sQLiteColumn.DataType);
+                */
+
+                /* SQLiteRow sQLiteRow = new SQLiteRow(3);
+                 foreach (var item in sQLiteRow)
+                 {
+                     Console.WriteLine(item);
+                 }*/
+                //Console.WriteLine(sQLiteRow[0]);
+                //Console.WriteLine(sQLiteRow["id"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SQLiteORM/SQLiteTable.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteORM
+{
+    class SQLiteTable
+    {
+        private string _name;
+        private SQLiteRow _headRow;
+        private SortedList<long, List<string>> _bodyRows;
+        public SQLiteRow HeadRowInfo
+        {
+            get
+            {
+                return _headRow;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public SortedList<long, List<string>> BodyRows {
+            get
+            {
+                return _bodyRows;
+            }
+        }
+        public SQLiteTable(string tableName, SQLiteRow headRow, SortedList<long, List<string>> bodyRows)
+        {
+            _name = tableName; //валидация?
+            _headRow = headRow;
+            _bodyRows = bodyRows;
+        }
+
+        public void RemoveRowById(long id)
+        {          
+            SQLiteConnector.Connection.Open();
+            string queryGetTablesData = $"DELETE FROM {_name} WHERE id = {id}";
+            SQLiteCommand sQLiteCommand = new SQLiteCommand(queryGetTablesData, SQLiteConnector.Connection);
+            int deletedCount = sQLiteCommand.ExecuteNonQuery();
+            Console.WriteLine( "deleted " + deletedCount + " row(s)" );
+            SQLiteConnector.Connection.Close();
+            _bodyRows.Remove(id);
+        }
+
+       public void CreateRow(List<string> row)
+        {          
+            SQLiteConnector.Connection.Open();
+            string queryGetTablesData = $"INSERT INTO {_name} (";  // делаем текст запроса        
+
+            foreach (SQLiteColumn column in _headRow)
+            {
+                if (!column.IsPrimaryKey)
+                {
+                    queryGetTablesData += $"'{column.Name}',";
+                }
+            }
+            queryGetTablesData = queryGetTablesData.Substring(0, queryGetTablesData.Length - 1);
+            queryGetTablesData += ") VALUES (";
+
+            foreach (string columnValue in row)
+            {                
+                queryGetTablesData += $"'{columnValue}',";               
+            }
+            queryGetTablesData = queryGetTablesData.Substring(0, queryGetTablesData.Length - 1);
+            queryGetTablesData += ")";
+
+            SQLiteCommand sQLiteCommand = new SQLiteCommand(queryGetTablesData, SQLiteConnector.Connection);
+            int insertedCount = sQLiteCommand.ExecuteNonQuery();//запуск sql запрос и возвращает сколько строк добавлено
+            Console.WriteLine("inserted " + insertedCount + " row(s)");
+            queryGetTablesData = $"SELECT MAX(id) FROM {_name}";// возвращает только что добавленный id студента
+            sQLiteCommand = new SQLiteCommand(queryGetTablesData, SQLiteConnector.Connection);
+            long lastId = (long)sQLiteCommand.ExecuteScalar();//возврашает одну ячейку первого рядка первого столбца           
+            SQLiteConnector.Connection.Close();
+
+            _bodyRows.Add(lastId, row);//SortedList
+
+        }
+       
+        //UPDATE student SET fio = 'DFcz', age = '24' WHERE id = 16;
+        public void UpdateRow(long id, List<string> row)
+        {
+            SQLiteConnector.Connection.Open();
+            string queryGetTablesData = $"UPDATE {_name} SET ";//делаем текст запроса
+            int i = 0;
+            foreach (SQLiteColumn column in _headRow)//column - данные о столбце (cid, Name, IsPrimaryKey, .....), _headRow - все столбцы - columns
+            {
+                if (!column.IsPrimaryKey)
+                {
+                    queryGetTablesData += $"{column.Name} = '{row[i]}',"; // {column.Name} = '{row[i]}',    -     fio = 'DFcz',
+                    i++;
+                }
+               
+            }
+            queryGetTablesData = queryGetTablesData.Substring(0, queryGetTablesData.Length - 1);           
+            queryGetTablesData += $" WHERE id = {id} ";
+
+            Console.WriteLine(queryGetTablesData);
+
+            SQLiteCommand sQLiteCommand = new SQLiteCommand(queryGetTablesData, SQLiteConnector.Connection);
+            int updatedCount = sQLiteCommand.ExecuteNonQuery();//запуск sql запрос и возвращает сколько строк добавлено
+            Console.WriteLine("updated " + updatedCount + " row(s)");
+            SQLiteConnector.Connection.Close();
+
+            _bodyRows[id] = row;
+        }
+
+
+
+    }
+}
